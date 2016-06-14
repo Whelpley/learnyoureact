b890f67b96ad4daf8287af5ad049e13512389d76
Whelpley <whelpley@gmail.com>
2016-06-14 15:03:09 -0700

    Complete tutorial

--- views/index.jsx
+++ views/index.jsx
@@ -1,129 +1,115 @@
-import React from 'react';
+let React = require('react');
 
-export default class TodoBox extends React.Component {
-    render() {
-        return (
-          <div className="todoBox">
-            <h1>Todos</h1>
-            <TodoList data = {this.props.data} />
-            <TodoForm />
-          </div>
-        );
-    }
-}
-
-// old code
-// ---
-// class TodoList extends React.Component {
-//     render() {
-//         var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
-//         return (
-//           <div className = "todoList">
-//             <table style={{border: "2px solid black"}}>
-//               <tbody>
-//                 {todo}
-//               </tbody>
-//             </table>
-//           </div>
-//         );
-//     }
-// }
-
-class TodoList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: this.props.data,
-            titleValue: "",
-            detailValue: ""
-        };
-        this.changeTitle = this.changeTitle.bind(this);
-        this.changeDetail = this.changeDetail.bind(this);
-        this.addTodo = this.addTodo.bind(this);
-    }
-
-    changeTitle(e) {
-      this.setState({titleValue: e.target.value});
-    }
-
-    changeDetail(e) {
-      this.setState({detailValue: e.target.value});
-    }
-
-    addTodo() {
-        let newData = this.state.data.concat({title: this.state.titleValue, detail: this.state.detailValue});
-        this.setState({data: newData});
-        this.setState({titleValue: ""});
-        this.setState({detailValue: ""});
-    }
-
-    render() {
-      let todo = this.state.data.map(function(obj) {
-        return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>;
-      });
-      return (
-        <div className = "todoList">
-          <div>
-            Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
-            Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
-            <button onClick={this.addTodo}>Add</button>
-          </div>
-          <table style={{border: "2px solid black"}}>
-            <tbody>
-              {todo}
-            </tbody>
-          </table>
-        </div>
-      );
+let TodoBox = React.createClass({
+  render: function () {
+    return (
+      <div className = "todoBox">
+        <h1>Todos</h1>
+        <TodoList data = {this.props.data} />
+        <TodoForm />
+      </div>
+    );
   }
-}
-
-var Todo = React.createClass({
-
-    propTypes: {
-        title: React.PropTypes.string.isRequired
-    },
-
-    getInitialState() {
-        return {checked: false};
-    },
-
-    handleChange() {
-        this.setState({checked: !this.state.checked});
-    },
-
-    render() {
-        let trStyle = this.state.checked ? style.checkedTodo : style.notCheckedTodo;
-        return (
-            <tr style={trStyle}>
-                <td style={style.tableContent}>
-                    <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
-                </td>
-                <td style={style.tableContent}>{this.props.title}</td>
-                <td style={style.tableContent}>{this.props.children}</td>
-            </tr>
-        );
-    }
 });
 
-class TodoForm extends React.Component {
-    render() {
-        return (
-            <div className="todoForm">
-              I am a TodoForm.
-            </div>
-        );
+var TodoList = React.createClass({
+  getInitialState: function() {
+    return {
+      data: this.props.data,
+      titleValue: "",
+      detailValue: ""
+    };
+  },
+  changeTitle: function(e) {
+    this.setState({titleValue: e.target.value});
+  },
+  changeDetail: function(e) {
+    this.setState({detailValue: e.target.value});
+  },
+  addTodo: function() {
+    let newData = this.state.data.concat({title: this.state.titleValue, detail: this.state.detailValue});
+    this.setState({data: newData});
+    this.setState({titleValue: ""});
+    this.setState({detailValue: ""});
+  },
+  deleteTodo: function(title) {
+    this.setState({
+      data: this.state.data.filter((todo) => {
+        return todo.title !== title;
+      })
+    });
+  },
+  render: function() {
+    var todo = this.state.data.map((obj) => {
+      return <Todo title={obj.title} key={obj.title} onDelete={this.deleteTodo}>{obj.detail}</Todo>;
+    });
+    return (
+      <div className = "todoList">
+        <div>
+          Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
+          Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} />
+          <button onClick={this.addTodo}>Add</button>
+        </div>
+        <table style={{border: "2px solid black"}}>
+          <tbody>
+            {todo}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
+
+let Todo = React.createClass({
+  propTypes: {
+    title: React.PropTypes.string.isRequired,
+    onDelete: React.PropTypes.func.isRequired
+  },
+  getInitialState: function () {
+    return {
+      checked: false,
+      tableStyle: style.tableContent
     }
-};
+  },
+  handleChange: function (e) {
+    this.setState({checked: e.target.checked});
+  },
+  _onDelete: function () {
+    this.props.onDelete(this.props.title);
+  },
+  render: function () {
+    let trStyle = this.state.checked ? style.checkedTodo : style.notCheckedTodo;
+    return (
+      <tr style = {trStyle}>
+        <td style = {style.tableContent}><button onClick={this._onDelete}>X</button></td>
+        <td style = {style.tableContent}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
+        <td style = {style.tableContent}>{this.props.title}</td>
+        <td style = {style.tableContent}>{this.props.children}</td>
+      </tr>
+    );
+  }
+});
+
+let TodoForm = React.createClass({
+  render: () => {
+    return (
+      <div className = "todoForm">
+        I am a TodoForm.
+      </div>
+    );
+  }
+});
 
 let style = {
-    checkedTodo: {
-        textDecoration: "line-through"
-    },
-    notCheckedTodo: {
-        textDecoration: "none"
-    },
-    tableContent: {
-        border: "1px solid black"
-    }
+  checkedTodo: {
+    textDecoration: "line-through"
+  },
+  notCheckedTodo: {
+    textDecoration: "none"
+  },
+  tableContent: {
+    border: "1px solid black"
+  }
 };
+
+export default TodoBox;
